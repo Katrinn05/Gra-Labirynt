0760029116becc7b3e078b734d884895214834d5
Katrinn05 <142241242+Katrinn05@users.noreply.github.com>
2023-11-16 23:55:38 +0100

    Create nawigacja.py

---
+++ nawigacja.py
@@ -0,0 +1,19 @@
+import msvcrt
+
+# Pozycja startowa
+x, y = 1, 1
+
+while True:
+  
+    # Odczytywanie ruchu gracza
+    key = msvcrt.getch().decode('utf-8').lower()
+
+    # Aktualizacja pozycji gracza
+    if key == 'w' and board[x-1][y] == 0:  # Góra
+        x -= 1
+    elif key == 's' and board[x+1][y] == 0:  # Dół
+        x += 1
+    elif key == 'a' and board[x][y-1] == 0:  # Lewo
+        y -= 1
+    elif key == 'd' and board[x][y+1] == 0:  # Prawo
+        y += 1
